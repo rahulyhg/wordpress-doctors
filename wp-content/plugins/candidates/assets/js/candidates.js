0756ee40c1353a61fe646195ed74e94ee9f8e5f5
d400800 <d400801@gmail.com>
2017-04-01 14:19:03 +0300

    adding dynamic controllers path for http requests

--- wp-content/plugins/candidates/assets/js/candidates.js
+++ wp-content/plugins/candidates/assets/js/candidates.js
@@ -1,7 +1,11 @@
+var ourLocation = window.location.href;
+var controllerPath = ( ourLocation.indexOf("benjamin400800.com") == -1 ) ? "/wp-content/plugins/candidates/controllers/" : "/doctors/wp-content/plugins/candidates/controllers/";
+console.log(controllerPath)
+
 var app = angular.module('myApp', []);
 app.controller('candidatesCtrl', function($scope, $http) {
 
-    $http.get("/wp-content/plugins/candidates/controllers/candidates-controller.php")
+    $http.get(controllerPath+"candidates-controller.php")
     .then(function (response) {
     	$scope.candidates = response.data;
     });
@@ -10,7 +14,7 @@ app.controller('candidatesCtrl', function($scope, $http) {
 
     $scope.updateStatus = function (x) {
     	$http({
-    		url: '/wp-content/plugins/candidates/controllers/status-controller.php',
+    		url: controllerPath+'status-controller.php',
     		method: "POST",
     		data: { 'id' : x.id, 'status' : x.status },
     		headers: {'Content-Type': 'application/x-www-form-urlencoded'},
