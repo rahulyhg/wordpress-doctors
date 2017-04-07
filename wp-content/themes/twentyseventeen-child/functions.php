765af2abe57cdfb95ffa09bd068508c08bf82361
d400800 <d400801@gmail.com>
2017-04-07 18:27:01 +0300

    adding paypal app

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -4,21 +4,14 @@
 	    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 	    // using style.css of the child theme
 	    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-	    wp_enqueue_style( 'child-style',
-	        get_stylesheet_directory_uri() . '/style.css',
-	        array( $parent_style ),
-	        $ver = null
-	    );
+	    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), $ver = null);
+	    wp_enqueue_style( 'bootstrap-grid', get_stylesheet_directory_uri() . '/bootstrap-grid.css', array(), $ver = null);
 	}
 	add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 	// registering javascript files
 	function my_theme_enqueue_js() {
-	    wp_enqueue_script(
-	        'child-custom-js',
-	        get_stylesheet_directory_uri() . '/js/custom.js',
-	        array(),
-	        $ver = null
-	    );
+	    wp_enqueue_script('child-custom-js', get_stylesheet_directory_uri() . '/js/custom.js', array(), $ver = null);
+	    wp_enqueue_script('paypal-checkout', 'https://www.paypalobjects.com/api/checkout.js', array(), $ver = null);
 	}
 	add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_js' );
--- wp-content/themes/twentyseventeen-child/js/custom.js
+++ wp-content/themes/twentyseventeen-child/js/custom.js
@@ -1,20 +1,75 @@
 var ourLocation = window.location.href;
 var controllerUrl = ( ourLocation.indexOf("benjamin400800.com") == -1 ) ? "/wp-content/themes/twentyseventeen-child/controllers/registration-controller.php" : "/doctors//wp-content/themes/twentyseventeen-child/controllers/registration-controller.php";
 
+function sendFormData (formSubmitted) {
+	jQuery.ajax({
+	    url: controllerUrl,
+	    type: "POST",
+	    data: formSubmitted.serialize(), 
+	    beforeSend: function(){
+	    	console.log("before sending request")
+	    },
+	    success: function(data) {
+	    	console.log(data)
+	    	jQuery(".paypal-container").slideDown(800)
+			renderPayPalBtn ()
+	    }
+	});
+}
+
+function renderPayPalBtn () {
+	paypal.Button.render({
+	    
+	    env: 'sandbox', // Optional: specify 'production' environment
+
+	    client: {
+	        sandbox:    'AZnETT6Z2J_PVghn5W69O4MGJdVOv96XPGA0JzhBGDtGL9anUzq2jPvIj5W27_5jC60hMla64scMAJVf',
+	        production: 'ARWrLg9684h8JseHdBWNbONelbzpGkGRbY6vNHUL-Ifa6CZWXChUUzc8Y-pnHvIoUB3k1OuPWcYpPz83'
+	    },
+
+	     style: {
+            label: 'checkout',	// checkout || credit
+            size:  'medium',    // tiny | small | medium
+            shape: 'rect',      // pill | rect
+            color: 'gold'       // gold | blue | silver
+        },
+
+	    payment: function() {
+	    
+	        var env    = this.props.env;
+	        var client = this.props.client;
+	    
+	        return paypal.rest.payment.create(env, client, {
+	            transactions: [
+	                {
+	                    amount: { total: '0.50', currency: 'ILS' }
+	                }
+	            ]
+	        });
+	    },
+
+	    commit: true, // Optional: show a 'Pay Now' button in the checkout flow
+
+	    onAuthorize: function(data, actions) {
+	    
+	        // Optional: display a confirmation page here
+	    
+	        return actions.payment.execute().then(function() {
+	            location = "http://benjamin400800.com/doctors/wp-admin";
+	        });
+	    }
+
+	}, '#paypal-button');
+}
+
 jQuery( document ).ready(function() {
-	jQuery(".wpcf7").on('wpcf7:mailsent', function(event){
-		jQuery.ajax({
-		    url: controllerUrl,
-		    type: "POST",
-		    data: jQuery(this).find(".wpcf7-form").serialize(), 
-		    beforeSend: function(){
-		    	console.log("before sending request")
-		    },
-		    success: function(data) {
-		        console.log(data)
-		    } 
-		});
-		console.log("form sent")
+	jQuery(".wpcf7").on('wpcf7:mailsent', function(event) {
+		sendFormData(jQuery(".wpcf7-form"));
+		jQuery(".wpcf7").slideUp(800);
+		// jQuery(".paypal-container").slideDown(800)
+		// renderPayPalBtn ()
 		//location = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=GLEFQQ8ZKQKT8';
 	});
+
 });
+
