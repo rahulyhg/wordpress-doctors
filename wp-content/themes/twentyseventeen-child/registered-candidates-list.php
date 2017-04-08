ef8e22ff8a1a6017c089847e73f61966a3365775
d400800 <d400801@gmail.com>
2017-04-08 20:46:52 +0300

    adding recent posts sidebar

--- wp-content/themes/twentyseventeen-child/registered-candidates-list.php
+++ wp-content/themes/twentyseventeen-child/registered-candidates-list.php
@@ -17,32 +17,53 @@ $candidates = $wpdb->get_results ("
 	ORDER BY `registration_date` DESC
 ");
 
+$args = array( 'posts_per_page' => 5 );
+$lastposts = get_posts( $args );
+
 get_header(); ?>
 
 <div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<?php if($candidates): ?>
-			<table class="candidates-table">
-				<tr>
-				    <th>Full name</th>
-					<th>Email</th>
-					<th>Registration date</th>
-				</tr>
-				<?php foreach ($candidates as $candidate): ?>
-					<tr>
-						<td><?=$candidate->name ." ". $candidate->surname; ?></td>
-						<td><?=$candidate->email; ?></td>
-						<td><?=$candidate->registration_date; ?></td>
-					</tr>					
-				<?php endforeach; ?>
-			</table>
-			<?php else: ?>
-				<p>Here will be the list of the registered candidates, when there are such.</p>
-			<?php endif; ?>
+			
+			<section class="row">
+				<div class="col-9">
+					<?php if($candidates): ?>
+					<table class="candidates-table">
+						<tr>
+						    <th>Full name</th>
+							<th>Email</th>
+							<th>Registration date</th>
+						</tr>
+						<?php foreach ($candidates as $candidate): ?>
+							<tr>
+								<td><?=$candidate->name ." ". $candidate->surname; ?></td>
+								<td><?=$candidate->email; ?></td>
+								<td><?=$candidate->registration_date; ?></td>
+							</tr>					
+						<?php endforeach; ?>
+					</table>
+					<?php else: ?>
+						<p>Here will be the list of the registered candidates, when there are such.</p>
+					<?php endif; ?>
+				</div>
+				<div class="col-3">
+					<div class="sidebar-posts">
+						<h2>Recent stories:</h2>
+						<?php foreach ( $lastposts as $post ): setup_postdata( $post ); ?>
+							<div class="sidebar-post-thumbnail">
+								<a style="background-image: url(<?php the_post_thumbnail_url('medium_large'); ?>)" href="<?php the_permalink(); ?>"></a>
+							</div>
+							<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+						<?php endforeach; 
+						wp_reset_postdata(); ?>
+					</div>
+				</div>
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 </div><!-- .wrap -->
+
 
 <?php get_footer();
